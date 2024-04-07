7675c9a2f0ecfa2296fe0514085a9de33a48419c
Patrickyoutubes <117463029+Patrickyoutubes@users.noreply.github.com>
2024-04-07 16:13:14 +0700

    Update glios6.lua

--- glios6.lua
+++ glios6.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Patrickyoutubes/SUPGUYS/main/glios7.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Patrickyoutubes/SUPGUYS/main/glios8.lua"))()
